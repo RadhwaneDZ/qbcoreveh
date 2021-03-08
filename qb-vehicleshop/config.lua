fa0ae1cc9a37deee93eb2e3750731a4eaaafd8f8
RadhwaneDZ <71345996+RadhwaneDZ@users.noreply.github.com>
2021-03-08 18:40:39 +0100

    Add files via upload

---
+++ qb-vehicleshop/config.lua
@@ -0,0 +1,112 @@
+QB = {}
+
+QBCustom = {}
+
+QB.VehicleShops = {
+    {x = -56.71, y = -1096.65, z = 25.44}
+}
+
+QB.GarageLabel = {
+    ["motelgarage"] = "Motel Garage",
+    ["sapcounsel"]  = "San Andreas Parking Counsel",
+}
+
+QB.SpawnPoint = {x = -59.18, y = -1109.71, z = 25.45, h = 68.5}
+QB.DefaultGarage = "centralgarage"
+
+QB.QuickSell = {x = -46.92, y = -1081.66, z = 26.74, h = 252.5, r = 1.0}
+
+QB.ShowroomVehicles = {
+       [1] = {
+        coords = {x = -50.66, y = -1093.05, z = 25.44, h = 222.5},
+        defaultVehicle = "17octavia",
+        chosenVehicle = "17octavia",
+        inUse = false,
+    },
+}
+
+QB.VehicleMenuCategories = {
+    ["sports"]  = {label = "Sports"},
+    ["super"]   = {label = "Super"},
+    ["sedans"]  = {label = "Sedans"},
+    ["coupes"]  = {label = "Coupes"},
+    ["suvs"]    = {label = "SUV's"},
+    ["offroad"] = {label = "Offroad"},
+	["compacts"]  = {label = "Compacts"},
+}
+
+QB.Classes = {
+    [0] = "compacts",  
+    [1] = "sedans",  
+    [2] = "suvs",  
+    [3] = "coupes",  
+    [4] = "muscle",  
+    [5] = "sportsclassics ", 
+    [6] = "sports",  
+    [7] = "super",  
+    [8] = "motorcycles",  
+    [9] = "offroad", 
+    [10] = "industrial",  
+    [11] = "utility",  
+    [12] = "vans",  
+    [13] = "cycles",  
+    [14] = "boats",  
+    [15] = "helicopters",  
+    [16] = "planes",  
+    [17] = "service",  
+    [18] = "emergency",  
+    [19] = "military",  
+    [20] = "commercial",  
+    [21] = "trains",  
+}
+
+QB.DefaultBuySpawn = {x = -56.79, y = -1109.85, z = 26.43, h = 71.5}
+
+QBCustom.VehicleBuyLocation = {x = -772.82, y = -235.39, z = 37.07, h = 201.5, r = 1.0}
+QBCustom.ShowroomPositions = {
+    [1] = {
+        coords = {
+            x = -792.46, 
+            y = -233.15, 
+            z = 36.05, 
+            h = 80.0,
+        },
+        vehicle = "718boxster",
+        buying = false,
+    }, 
+    [2] = {
+        coords = {
+            x = -789.57, 
+            y = -237.95, 
+            z = 36.05, 
+            h = 83.5, 
+        }, 
+        vehicle = "hevo",
+        buying = false,
+    }, 
+    [3] = {
+        coords = {
+            x = -786.98, 
+            y = -242.74, 
+            z = 36.05, 
+            h = 74.5,
+        },
+        vehicle = "r820",
+        buying = false,
+    }, 
+    [4] = {
+        coords = {
+            x = -783.27, 
+            y = -223.07, 
+            z = 36.36, 
+            h = 138.5,
+        },
+        vehicle = "macla",
+        buying = false,
+    },
+    [5] = {
+        coords = {x = -793.74, y = -229.36, z = 36.07, h = 74.5},
+        vehicle = "urus",
+        buying = false,
+    },
+}
